fc29e591a769e57f0f981dd397c64d2ea968d151
lainlynr <manifure@gmail.com>
2024-07-24 21:37:15 +0300

    add pagination

--- src/main.go
+++ src/main.go
@@ -13,13 +13,25 @@ import (
 
 type Search struct {
 	SearchKey    string
+	Result       []SteamAPI.SteamApp
 	NextPage     int
 	TotalPages   int
 	TotalResults int
-	Result       []SteamAPI.SteamApp
+	CurrentPage  int
+	HasNextPage  bool
+	HasPrevPage  bool
 }
 
-var tpl = template.Must(template.ParseFiles("html/home.html"))
+var funcMap = template.FuncMap{
+	"sub": func(a, b int) int {
+		return a - b
+	},
+	"add": func(a, b int) int {
+		return a + b
+	},
+}
+
+var tpl = template.Must(template.New("home.html").Funcs(funcMap).ParseFiles("html/home.html"))
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
 	u, err := url.Parse(r.URL.String())
@@ -36,13 +48,14 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	search := &Search{}
 	search.SearchKey = searchKey
 
-	next, err := strconv.Atoi(page)
+	currentPage, err := strconv.Atoi(page)
 	if err != nil {
-		http.Error(w, "Unexpected server error", http.StatusInternalServerError)
+		http.Error(w, "Invalid page number", http.StatusInternalServerError)
 		return
 	}
 
-	search.NextPage = next
+	itemsPerPage := 48
+	offset := (currentPage - 1) * itemsPerPage
 
 	psqlInfo := SqlFunc.GetPsqlInfo()
 	db, err := sql.Open("postgres", psqlInfo)
@@ -51,9 +64,16 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	const query = "SELECT * FROM steam_games WHERE name ILIKE $1 limit 48"
+	countQuery := "SELECT COUNT(*) FROM steam_games WHERE name ILIKE $1"
 	searchPattern := "%" + search.SearchKey + "%"
-	res, err := db.Query(query, searchPattern)
+	var count int
+	err = db.QueryRow(countQuery, searchPattern).Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := "SELECT * FROM steam_games WHERE name ILIKE $1 LIMIT $2 OFFSET $3"
+	res, err := db.Query(query, searchPattern, itemsPerPage, offset)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,11 +92,27 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	search.Result = showApp
-	search.TotalResults = len(showApp)
+	search.TotalResults = count
+	search.CurrentPage = currentPage
+	search.TotalPages = (count + itemsPerPage - 1) / itemsPerPage
+	search.HasNextPage = currentPage < search.TotalPages
+	search.HasPrevPage = currentPage > 1
+
 	err = tpl.Execute(w, search)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func parseIntOrDefault(s string, def int) (int, error) {
+	if s == "" {
+		return def, nil
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, err
+	}
+	return i, nil
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
